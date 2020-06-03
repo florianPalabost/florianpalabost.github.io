b89a62361d501b3603cdd26a5991428cf725fddf
Florian P <florian69780@hotmail.fr>
2020-06-03 14:54:22 +0200

    [features][skill_list][contact_form] features skill_list & contact_form done

--- src/app/contact-component/contact-component.component.ts
+++ src/app/contact-component/contact-component.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {faStar, faEnvelope} from '@fortawesome/free-solid-svg-icons';
 import {faSkype} from '@fortawesome/free-brands-svg-icons';
+import {FormBuilder} from "@angular/forms";
+import {environment} from "../../environments/environment";
+declare let emailjs: any;
 
 @Component({
   selector: 'app-contact-component',
@@ -11,9 +14,25 @@ export class ContactComponentComponent implements OnInit {
   faStar = faStar;
   faSkype = faSkype;
   faEnvelope = faEnvelope;
-  constructor() { }
-
-  ngOnInit() {
+  formContact;
+  constructor(private fb: FormBuilder) {
+    this.formContact =  this.fb.group({
+      contact_number: '',
+      user_name: '',
+      user_email: '',
+      message: ''
+    });
   }
 
+  ngOnInit() {
+    emailjs.init(environment.user_id);
+  }
+
+  async sendMsg() {
+    const response = await emailjs.send('gmail', 'contact_form', this.formContact.value);
+    if(response.status === 200 && response.text === 'OK') {
+      alert('Message envoyé !');
+    }
+
+  }
 }
