72d9670d04718d9703d938bc9d7e2c696635d774
Florian P <florian69780@hotmail.fr>
2020-06-24 12:15:23 +0200

    [features/login_logout] F done

--- src/app/nav-component/nav-component.component.ts
+++ src/app/nav-component/nav-component.component.ts
@@ -1,21 +1,43 @@
-import { Component, OnInit } from "@angular/core";
-import { TranslateService } from "@ngx-translate/core";
-
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { OktaAuthService } from '@okta/okta-angular';
+import appConfig from '../app.config';
 @Component({
-  selector: "app-nav-component",
-  templateUrl: "./nav-component.component.html",
-  styleUrls: ["./nav-component.component.scss"]
+  selector: 'app-nav-component',
+  templateUrl: './nav-component.component.html',
+  styleUrls: ['./nav-component.component.scss']
 })
 export class NavComponentComponent implements OnInit {
-  constructor(public translate: TranslateService) {
-    translate.addLangs(["fr", "en"]);
-    translate.setDefaultLang("fr");
-    translate.use("fr");
+  isAuthenticated: boolean;
+
+  constructor(public translate: TranslateService, private oktaAuth: OktaAuthService) {
+    translate.addLangs(['fr', 'en']);
+    translate.setDefaultLang('fr');
+    translate.use('fr');
+
+    this.oktaAuth.$authenticationState.subscribe(isAuthenticated => (this.isAuthenticated = isAuthenticated));
   }
 
-  ngOnInit() {}
+  async ngOnInit() {
+    this.isAuthenticated = await this.oktaAuth.isAuthenticated();
+  }
 
   useLanguage(language: string) {
     this.translate.use(language);
+  }
+
+  async logout() {
+    // Read idToken before local session is cleared
+    const idToken = await this.oktaAuth.getIdToken();
+
+    // Clear local session
+    await this.oktaAuth.logout('/');
+
+    // Clear remote session
+    window.location.href = `${appConfig.oidc.issuer}/v1/logout?id_token_hint=${idToken}&post_logout_redirect_uri=http://localhost:4200`;
+  }
+
+  login() {
+    this.oktaAuth.loginRedirect('login');
   }
 }
